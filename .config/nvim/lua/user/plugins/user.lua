f89d7083fd7e7796b780eda7b535b410d5fe3de4
Yiming zhang <592148382@qq.com>
2023-11-05 12:59:36 +0800

    remove duplicate lsp-signature config

--- .config/nvim/lua/user/plugins/user.lua
+++ .config/nvim/lua/user/plugins/user.lua
@@ -2,12 +2,6 @@ return {
 
   -- You can also add new plugins here as well:
   -- Add plugins, the lazy syntax
-  "andweeb/presence.nvim",
-  {
-    "ray-x/lsp_signature.nvim",
-    event = "BufRead",
-    config = function() require("lsp_signature").setup() end,
-  },
   {
     "ellisonleao/gruvbox.nvim",
     priority = 1000,
