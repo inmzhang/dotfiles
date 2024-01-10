459a22acfc2d6fc87fb8b68ab963c3d1bdfea7d4
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2024-01-10 13:13:19 +0800

    use copilot-lua-cmp and change conflict keybind with neorg

--- .config/nvim/lua/user/plugins/community.lua
+++ .config/nvim/lua/user/plugins/community.lua
@@ -4,8 +4,9 @@ return {
   -- example of imporing a plugin, comment out to use it or add your own
   -- available plugins can be found at https://github.com/AstroNvim/astrocommunity
   { import = "astrocommunity.pack.rust" },
+
   { import = "astrocommunity.pack.cpp" },
-  { import = "astrocommunity.completion.copilot-lua" },
+  { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.motion.leap-nvim" },
   { import = "astrocommunity.motion.nvim-surround" },
   { import = "astrocommunity.markdown-and-latex.markdown-preview-nvim" },
--- .config/nvim/lua/user/plugins/neorg.lua
+++ .config/nvim/lua/user/plugins/neorg.lua
@@ -13,27 +13,25 @@ return {
         ["core.concealer"] = {
           config = {
             icon_preset = "diamond",
-          }
+          },
         }, -- Adds pretty icons to your documents
         ["core.export"] = {},
         ["core.itero"] = {},
         ["core.keybinds"] = {
           config = {
-              hook = function(keybinds)
-                keybinds.remap_key("norg", "i", "<M-CR>", "<C-j>")
-              end,
-          }
+            hook = function(keybinds) keybinds.remap_key("norg", "i", "<M-CR>", "<C-n>") end,
+          },
         },
         ["core.completion"] = {
           config = {
             engine = "nvim-cmp",
-          }
+          },
         },
         ["core.dirman"] = { -- Manages Neorg workspaces
           config = {
             workspaces = {
               main = "~/neorg/main",
-              root = "~/neorg"
+              root = "~/neorg",
             },
             default_workspace = "main",
           },
@@ -41,8 +39,8 @@ return {
         ["core.journal"] = {
           config = {
             workspace = "root",
-          }
-        }
+          },
+        },
       },
     }
   end,
