75ce3f4ec54270e30d6565605cf01e4fd37df268
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2024-02-11 21:34:48 +0800

    clear all files

--- .config/shell/vendors.sh
+++
@@ -1,7 +0,0 @@
-eval "$(zoxide init zsh)"
-
-eval "$(pyenv init -)"
-
-eval "$(atuin init zsh)"
-
-eval "$(starship init zsh)"
--- .config/shell/zap_plugins.sh
+++
@@ -1,5 +0,0 @@
-# Plugins managed by zap
-[ -f "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh" ] && source "${XDG_DATA_HOME:-$HOME/.local/share}/zap/zap.zsh"
-plug "zsh-users/zsh-autosuggestions"
-plug "zsh-users/zsh-syntax-highlighting"
-plug "zsh-users/zsh-history-substring-search"
