0d25cd884ad0f38b0d243077e7a4a7d656272ed6
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2024-02-12 18:31:44 +0800

    remove unnecessary envars

--- dotfiles/zsh/envars.sh
+++ dotfiles/zsh/envars.sh
@@ -1,11 +1,5 @@
 # cargo
 export PATH="$HOME/.cargo/bin:$PATH"
-
-# pyenv
-export PYENV_ROOT="$HOME/.pyenv"
-[[ -d $PYENV_ROOT/bin ]] && export PATH="$PYENV_ROOT/bin:$PATH"
-
-export EDITOR="nvim"
  
 # Julia mirror
 export JULIA_PKG_SERVER=https://mirrors.ustc.edu.cn/julia
