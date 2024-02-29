197c7b2d7a160c8f41041fbc5840a3e0fca6b5f9
Yiming Zhang <592148382@qq.com>
2024-02-29 22:07:19 +0800

    replace pyright with ruff_lsp

--- dotfiles/nvim/user/plugins/mason.lua
+++ dotfiles/nvim/user/plugins/mason.lua
@@ -5,7 +5,7 @@ return {
     "williamboman/mason-lspconfig.nvim",
     -- overrides `require("mason-lspconfig").setup(...)`
     opts = {
-      ensure_installed = { "marksman", "lua_ls", "rust_analyzer", "pyright" },
+      ensure_installed = { "marksman", "lua_ls", "rust_analyzer", "ruff_lsp" },
     },
   },
   -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
