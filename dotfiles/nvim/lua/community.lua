9077bd0499e9aa66b959f39b7ddf1c47323a0902
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2025-06-24 14:19:44 +0800

    renable cpp pack

--- dotfiles/nvim/lua/community.lua
+++ dotfiles/nvim/lua/community.lua
@@ -9,6 +9,7 @@ return {
 	{ import = "astrocommunity.pack.rust" },
 	{ import = "astrocommunity.pack.python-ruff" },
 	{ import = "astrocommunity.pack.typst" },
+	{ import = "astrocommunity.pack.cpp" },
 	{ import = "astrocommunity.motion.leap-nvim" },
 	{ import = "astrocommunity.motion.nvim-surround" },
 	{ import = "astrocommunity.search.nvim-hlslens" },
