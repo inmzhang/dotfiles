d0d2162e67a94f1fb8eeaa78075334e0a76f0165
Yiming Zhang <zhangyiming21@mail.ustc.edu.cn>
2025-11-16 18:25:35 +0800

    update config

--- home.nix
+++ home.nix
@@ -100,7 +100,6 @@ in
     userName = "Yiming Zhang";
     userEmail = "zhangyiming21@mail.ustc.edu.cn";
     delta.enable = true;
-    extraConfig.credential.helper = "store";
     aliases = {
       lg = "lg1";
       lg1 = "lg1-specific --all";
@@ -109,6 +108,11 @@ in
       lg1-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)'";
       lg2-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
       lg3-specific = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset) %C(bold cyan)(committed: %cD)%C(reset) %C(auto)%d%C(reset)%n''          %C(white)%s%C(reset)%n''          %C(dim white)- %an <%ae> %C(reset) %C(dim white)(committer: %cn <%ce>)%C(reset)'";
+    };
+    extraConfig = {
+      credential.helper = "store";
+      http.proxy = "http://127.0.0.1:7897";
+      https.proxy = "http://127.0.0.1:7897";
     };
   };
   # others
